ff7d67f6442d96ce4e78061ee423bb5bc886d343
GhaidaMousa <Ghaida7002@gmail.com>
2025-02-19 10:06:28 +0300

    organizing files and editing the page

--- public/code.js
+++ public/code.js
@@ -36,7 +36,10 @@
   const hiddenElements = document.querySelectorAll('.hiddenSec');
   hiddenElements.forEach((el) => observer.observe(el));
   */
+ // })
   ////////////////// delete above if not using
+
+// to highlghit nav items when a press on it  
 $('header .nav-link').click(function(){
   $('header .nav-link').removeClass('active');
   $('header .nav-link').addClass('text-secondary');
@@ -44,6 +47,42 @@ $('header .nav-link').click(function(){
   $(this).addClass('active');
 })
 
+// to add fade effect on i scroll to section 
+// const observer = new IntersectionObserver((entries)=>{
+//   entries.forEach((entry)=>{
+//     console.log(entry)
+//     if(entry.isIntersecting){
+//       entry.target.classList.add('showSection')
+//     }
+//   });
+// });
 
+// $('.hiddenSection').each((el)=> observer.observe($(this)));
 
-// })
+/////
+
+// const sectionObserver = new IntersectionObserver((entries)=>{
+//     entries.forEach((entry)=>{
+//       console.log(entry)
+//       if(entry.isIntersecting){
+//         $(this).fadeIn('slow');     
+//        }
+//     });
+//   });
+  // sectionObserver.observe($('section')[0]);
+  // $('section').each((el)=> {
+  //   sectionObserver.observe(el).$(this)})
+  $('.hero').animate({'opacity':'1'},1000);
+  $('main').scroll(function(){
+    $('.fadein').each( function(i){
+      const bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      const bottom_of_window = $('main').scrollTop() + $('main').height();
+      console.log(bottom_of_element , "|||", bottom_of_window);
+      console.log($(this))
+
+      if(bottom_of_window > bottom_of_element){
+        $(this).animate({'opacity':'1'},1000);
+        // $(this).fadeIn('slow');  
+      }
+    });
+  });
